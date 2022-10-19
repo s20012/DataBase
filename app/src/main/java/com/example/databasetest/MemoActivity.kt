44a1ddc527436a6b16feedd6d6bb192257f324c6
s20012 <s20012@std.it-college.ac.jp>
2022-10-19 16:31:58 +0900

    音声入力

--- app/src/main/java/com/example/databasetest/MemoActivity.kt
+++ app/src/main/java/com/example/databasetest/MemoActivity.kt
@@ -2,21 +2,18 @@ package com.example.databasetest
 
 import android.Manifest.permission.RECORD_AUDIO
 import android.app.AlertDialog
-import android.app.Dialog
 import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.speech.RecognitionListener
 import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.databasetest.databinding.ActivityMainBinding
 import com.example.databasetest.databinding.ActivityMemoBinding
 
 
@@ -38,12 +35,30 @@ class MemoActivity : AppCompatActivity() {
         if (granted != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, arrayOf(RECORD_AUDIO), PERMISSIONS_RECORD_AUDIO)
         }
-        
+        val editText = AppCompatEditText(this)
 
         speechRecognizer = SpeechRecognizer.createSpeechRecognizer(applicationContext)
-        speechRecognizer?.setRecognitionListener(createRecognitionListenerStringStream {  binding.textContent.setText(it) })
-        binding.inputText.setOnClickListener { speechRecognizer?.startListening(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)) }
-        binding.button2.setOnClickListener { speechRecognizer?.stopListening() }
+        speechRecognizer?.setRecognitionListener(createRecognitionListenerStringStream {  editText.setText(it) })
+
+
+
+        binding.daialog.setOnClickListener {
+                speechRecognizer?.startListening(Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH))
+                AlertDialog.Builder(this)
+                    .setTitle("入力中...")
+                    .setView(editText)
+                    .setPositiveButton("保存") { dialog, _ ->
+                        speechRecognizer?.stopListening()
+                        binding.textContent.text = editText.text
+                        dialog.dismiss()
+                    }
+                    .create()
+                    .show()
+
+
+        }
+
+
         val memoId: Long = intent.getLongExtra("id",0)
         if (memoId != 0L) {
             helper.readableDatabase.use {
@@ -58,7 +73,7 @@ class MemoActivity : AppCompatActivity() {
             }
         }
 
-        findViewById<Button>(R.id.save_button).setOnClickListener{
+        binding.saveButton.setOnClickListener{
             helper.writableDatabase.use {
                     db ->
                 val values = ContentValues().apply {
@@ -73,7 +88,7 @@ class MemoActivity : AppCompatActivity() {
             }
             finish()
         }
-        findViewById<Button>(R.id.delete_button).setOnClickListener {
+        binding.deleteButton.setOnClickListener {
             helper.writableDatabase.use {
                     db ->
                 db.delete(TABLE_NAME, "id = ?", arrayOf(memoId.toString()))
@@ -81,7 +96,7 @@ class MemoActivity : AppCompatActivity() {
             }
             finish()
         }
-        findViewById<Button>(R.id.back_button).setOnClickListener {
+        binding.backButton.setOnClickListener {
             finish()
         }
     }
